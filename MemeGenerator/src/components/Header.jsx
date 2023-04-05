55dbf2a8d8abe02500d6e5b97e724b077b3e81ca
Yana Radeva <yana.radeva7@gmail.com>
2023-04-05 11:03:42 +0300

    create header component and style it

---
+++ MemeGenerator/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (    
+        <header>
+            <img src="src/assets/troll-face.png"></img>
+            <h2>Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
