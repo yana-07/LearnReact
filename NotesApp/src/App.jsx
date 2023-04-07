704a5aec5f9e12bc26c63537fc9a362382e57f05
Yana Radeva <yana.radeva7@gmail.com>
2023-04-07 11:34:00 +0300

    integrate initial project state

--- NotesApp/src/App.jsx
+++ NotesApp/src/App.jsx
@@ -1,15 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Sidebar from "./components/Sidebar"
+import {Editor} from "./components/Editor"
+import {data} from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-    </div>
+  const [notes, setNotes] = useState([])
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
   )
+    
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+    
+  function updateNote(text) {
+    setNotes(oldNotes => oldNotes.map(oldNote => {
+      return oldNote.id === currentNoteId
+        ? { ...oldNote, body: text }
+        : oldNote
+      }))
+  }
+    
+  function findCurrentNote() {
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+    
+  return (
+    <main>
+      {
+        notes.length > 0 
+        ?
+        <Split 
+          sizes={[30, 70]} 
+          direction="horizontal" 
+          className="split"
+        >
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {
+            currentNoteId && 
+            notes.length > 0 &&
+            <Editor 
+                currentNote={findCurrentNote()} 
+                updateNote={updateNote} 
+            />
+          }
+        </Split>
+        :
+        <div className="no-notes">
+            <h1>You have no notes</h1>
+            <button 
+                className="first-note" 
+                onClick={createNewNote}
+            >
+                Create one now
+            </button>
+        </div>
+            
+        }
+        </main>
+    )
 }
 
 export default App
