77614d00009aa29a271543b5b73cdf17a49dc356
Yana Radeva <yana.radeva7@gmail.com>
2023-04-07 10:08:09 +0300

    render Meme component inside the App component

--- MemeGenerator/src/App.jsx
+++ MemeGenerator/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Meme from './components/Meme'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Header />
+    <div>
+      <Header />
+      <Meme />
+    </div>
+    
   )
 }
 
