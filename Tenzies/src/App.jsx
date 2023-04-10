ac0cb0e30465835fb790e43c9291eeacefc85f0b
Yana Radeva <yana.radeva7@gmail.com>
2023-04-10 10:54:02 +0300

    add App component

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Die from "./components/Die"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(allNewDice)
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6 ),
+      isHeld: false
+    }
+  }
+
+  function allNewDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push(generateNewDie());
+    }
+
+    return dice;
+  }
+
+  const diceElements = dice.map(die => 
+      <Die 
+        value={die.value} 
+      />
+  ); 
 
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="dice-cotainer">
+        {diceElements}
+      </div>
+      <button>Roll</button>
+    </main>
   )
 }
 
