4b0cbc661a9972c6692d1523e30916071ff006cb
Yana Radeva <yana.radeva7@gmail.com>
2023-04-19 16:39:26 +0300

    add stateless and controlled ExpenseFilter component and start state management in Expenses component

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,22 +1,35 @@
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-import './Expenses.css'
+import { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
-function Expenses({items}) {
-    const expenseElements = items.map(expense => {
-        return <ExpenseItem 
-                 key={expense.id} 
-                 title={expense.title} 
-                 amount={expense.amount} 
-                 date={expense.date} 
-              />
-    })
+function Expenses({ items }) {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
+  const expenseElements = items.map((expense) => {
     return (
-        <Card className="expenses">
-            {expenseElements}
-        </Card>
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
     );
+  });
+
+  const changeYearHandler = (year) => {
+    setFilteredYear(year);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter filteredYear={filteredYear} onChangeYear={changeYearHandler} />
+        {expenseElements}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;
