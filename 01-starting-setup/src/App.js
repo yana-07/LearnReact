9abe056e68adb52fa5378dacaf5834eea67d422a
Yana Radeva <yana.radeva7@gmail.com>
2023-04-19 13:24:41 +0300

    add NewExpense component containing an ExpenseForm component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 //import React from 'react'
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -38,7 +39,7 @@ function App() {
 
   return (
     <div>
-      <h2>Lets get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
