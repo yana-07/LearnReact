42a2d72f0902f00c78826fbe16e0385855d13ba1
Yana Radeva <yana.radeva7@gmail.com>
2023-04-19 16:37:27 +0300

    remove state from component

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
-import { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 function ExpenseItem({title, amount, date}) {
-  const [expenseTitle, setExpenseTitle] = useState(title)
-
-  const clickHandler = () => {
-    setExpenseTitle("Updated");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
