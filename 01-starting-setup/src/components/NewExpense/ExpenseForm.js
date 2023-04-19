6c87dd293323beb42d651c7b4241dc5b9a8c9cbc
Yana Radeva <yana.radeva7@gmail.com>
2023-04-19 14:06:25 +0300

    manage user input using a single state object

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,54 @@
+import { useState } from 'react'
+
 import './ExpenseForm.css'
 
 function ExpenseForm() {
+    const [expense, setExpense] = useState({
+        title: '',
+        amount: '',
+        date: ''
+    })
+
+    const expenseChangeHandler = (event) => {
+        setExpense(prevExpense => ({
+            ...prevExpense,
+            [event.target.name]: event.target.value
+        }));
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(expense)
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input 
+                      type="text" 
+                      name="title" 
+                      onChange={expenseChangeHandler} 
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input 
+                      type="number" 
+                      name="amount" 
+                      min="0.01" step="0.01" 
+                      onChange={expenseChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31"/>
+                    <input 
+                      type="date" 
+                      name="date" 
+                      min="2019-01-01" max="2023-12-31" 
+                      onChange={expenseChangeHandler}
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
