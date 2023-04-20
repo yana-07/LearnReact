28df642f9774ce9498f7b888441509d3d12fd04c
Yana Radeva <yana.radeva7@gmail.com>
2023-04-20 17:46:02 +0300

    add inline styles depending on state

--- course-goal-items/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course-goal-items/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,33 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isVaid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isVaid ? 'red' : 'black'}}>Course Goal</label>
+        <input 
+          style={{backgroundColor: !isVaid ? 'pink' : 'transparent', borderColor: !isVaid ? 'red' : '#ccc'}} 
+          type="text" 
+          onChange={goalInputChangeHandler} 
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
