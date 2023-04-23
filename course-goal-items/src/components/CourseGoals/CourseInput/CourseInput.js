2d1d2a7d41f341f82ebe24f9b32a0af4da6381fe
Yana Radeva <yana.radeva7@gmail.com>
2023-04-23 11:18:00 +0300

    used props passed to styled components to dynamically change parts of the styles

--- course-goal-items/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course-goal-items/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
@@ -11,12 +10,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red' : 'black'};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    background-color: ${props => props.invalid ? 'rgb(241, 197, 204)' : 'transparent'}; 
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -27,20 +28,11 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-
-  &.invalid input {
-    border-color: red;
-    background-color: rgb(241, 197, 204);
-  }
-
-  &.invalid label {
-    color: red;
-  }
 `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-  const [isVaid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -60,7 +52,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isVaid && 'invalid'}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
