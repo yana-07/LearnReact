033262a8e48b71ab2e276c8ad1e05527122897c8
Yana Radeva <yana.radeva7@gmail.com>
2023-04-28 09:31:03 +0300

    use refs to read user input on submit instead of state being updated on every key stroke

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -6,19 +6,47 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
-    const [userInput, setUserInput] = useState({username: '', age: ''});
+    const usernameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    //const [userInput, setUserInput] = useState({username: '', age: ''});
     const [error, setError] = useState(); // initial state = undefined
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (userInput.username.trim().length === 0 || userInput.age.trim().length === 0) {
+        const eneteredUsername = usernameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
+        const input = {
+            username: eneteredUsername,
+            age: enteredAge
+        }
+
+        // if (userInput.username.trim().length === 0 || userInput.age.trim().length === 0) {
+        //     setError({
+        //         title: "Invalid input!",
+        //         message: "Username and age must be at least 1 character long."
+        //     });
+        //     return;
+        // }
+        // if (+userInput.age < 1) {
+        //     setError({
+        //         title: "Invalid age!",
+        //         message: "Cannot set a negative age."
+        //     });
+        //     return;
+        // }
+
+        // props.onAddUser(userInput);
+        // setUserInput({username: '', age: ''});
+
+        if (eneteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
             setError({
                 title: "Invalid input!",
                 message: "Username and age must be at least 1 character long."
             });
             return;
         }
-        if (+userInput.age < 1) {
+        if (+enteredAge.age < 1) {
             setError({
                 title: "Invalid age!",
                 message: "Cannot set a negative age."
@@ -26,16 +54,21 @@ const AddUser = (props) => {
             return;
         }
 
-        props.onAddUser(userInput);
-        setUserInput({username: '', age: ''});
+        props.onAddUser(input);
+
+        // One should very rarely use refs to manipulate the DOM (let React manage the DOM!)
+        // the following is ok, as we are not really manipulating the DOM in a sence of adding or emoving elements, but still
+        // this is not quite advised
+        usernameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
-    const userInputChangeHandler = (event) => {
-        setUserInput(prevUserInput => ({
-            ...prevUserInput,
-            [event.target.name]: event.target.value
-        }))
-    };
+    // const userInputChangeHandler = (event) => {
+    //     setUserInput(prevUserInput => ({
+    //         ...prevUserInput,
+    //         [event.target.name]: event.target.value
+    //     }))
+    // };
 
     const hideModalHandler = () => {
         setError(null);
@@ -51,16 +84,18 @@ const AddUser = (props) => {
                         id="username" 
                         name="username" 
                         type="text" 
-                        value={userInput.username} 
-                        onChange={userInputChangeHandler} 
+                        //value={userInput.username} 
+                        //onChange={userInputChangeHandler}
+                        ref={usernameInputRef} 
                     />
                     <label htmlFor="age">Age (Years)</label>
                     <input 
                         id="age" 
                         name="age" 
                         type="number" 
-                        value={userInput.age} 
-                        onChange={userInputChangeHandler} 
+                        //value={userInput.age} 
+                        //onChange={userInputChangeHandler} 
+                        ref={ageInputRef}
                     />
                     <Button type="submit" onClick={addUserHandler}>Add User</Button>
                 </form>
