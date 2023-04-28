3c2517533a005680e8503a05e066b61ffbed5f3c
Yana Radeva <yana.radeva7@gmail.com>
2023-04-28 13:56:28 +0300

    use useEffect hook to utilize storing user logged in info in the local storage

--- practice-project-2/src/App.js
+++ practice-project-2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // executed after each component re-evaluation but ONLY if there is a change in the dependencies
+  // if the dependencies are an empty array, the fn inside the useEffect will only run when the app starts / is reloaded
+  useEffect(() => {
+    const userLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if (userLoggedInInfo === '1') {
+      setIsLoggedIn(true);
+    } 
+  }, [isLoggedIn]);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
