4b23a4b59eecd1a83bc1a1102911364eab8073c7
Yana Radeva <yana.radeva7@gmail.com>
2023-05-02 14:06:23 +0300

    use useReducer to manage cart state in the CartProvider component

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,17 +1,49 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-    const addItemToCartHandler = (item) => {
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
 
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = [
+            action.item,
+            ...state.items
+        ]; // or state.items.concat(action.item) -> a non-mutator method - returns a new array
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD',
+            item
+        });
     };
 
     const removeItemFromCartHandler = (id) => {
-
+        dispatchCartAction({
+            type: 'REMOVE',
+            id
+        })
     };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
