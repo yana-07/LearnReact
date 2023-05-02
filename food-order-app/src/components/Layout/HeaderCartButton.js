9997c8197d0044fdddbf1df78be63c9a187b30a7
Yana Radeva <yana.radeva7@gmail.com>
2023-05-02 16:48:38 +0300

    manage btnIsHighlightedState as a side effect in response to a change in the cart items count to conditionally add a class to the button and trigger animation

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const cartItemsCount = cartCtx.items.reduce((accumulator, currItem) => {
+    const { items } = cartCtx;
+
+    const cartItemsCount = items.reduce((accumulator, currItem) => {
         return accumulator + currItem.amount
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        // if we add multiple items rapidly after each other, we must ensure the old timer is cleared and a new timer is set
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
