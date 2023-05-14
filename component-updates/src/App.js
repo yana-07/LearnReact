0e92878d48d0e0bf749a12f2bc02e64d909e692d
yana.radeva7 <yana.radeva7@gmail.com>
2023-05-14 13:16:41 +0300

    implement toggle paragraph functionality to demonstrate how components are re-evaluated when state changes and how changes to the real DOM are only made by React DOM for differences between evaluations (of the virtual DOM)

--- component-updates/src/App.js
+++ component-updates/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button'
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
-      <h1>Hi there!</h1>
+      {showParagraph && <h1>Hi there!</h1>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
