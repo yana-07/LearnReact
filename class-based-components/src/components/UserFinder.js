3df71d852466506c04ecdc46560dca0c0ca0d921
yana.radeva <yana.radeva7@gmail.com>
2023-10-24 17:50:03 +0300

    introduce error boundaries for class-based components

--- class-based-components/src/components/UserFinder.js
+++ class-based-components/src/components/UserFinder.js
@@ -3,6 +3,7 @@ import { Fragment, useState, useEffect, Component } from 'react';
 import Users from './Users';
 import classes from './UserFinder.module.css';
 import UsersContext from '../store/users-context';
+import ErrorBoundary from './ErrorBoundary';
 
 class UserFinder extends Component {
     //the static property can only be set once, so a class-based component is able to directly connect to one context only!
@@ -42,7 +43,9 @@ class UserFinder extends Component {
             <div className={classes.finder}>
               <input type="search" onChange={this.searchChangeHandler.bind(this)} />
             </div>
-            <Users users={this.state.filteredUsers} />
+            <ErrorBoundary>
+                <Users users={this.state.filteredUsers} />
+            </ErrorBoundary>
           </Fragment>
         );
     }
