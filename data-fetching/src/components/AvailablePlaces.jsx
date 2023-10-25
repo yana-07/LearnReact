3db939d0ffb9f2e964bcc951b5dd0d27cf1e9068
yana.radeva <yana.radeva7@gmail.com>
2023-10-25 11:35:40 +0300

    transform fetched data

--- data-fetching/src/components/AvailablePlaces.jsx
+++ data-fetching/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Erros.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -23,12 +24,20 @@ export default function AvailablePlaces({ onSelectPlace }) {
         }
 
         const resData = await response.json();
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || 'Could not fetch places, please try again later.' });
-      }
-      
-      setIsFetching(false);
+        setIsFetching(false);
+      }   
     }
 
     fetchPlaces();
