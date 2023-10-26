15211ade41cd62853461910bb0214e6d714e321d
yana.radeva <yana.radeva7@gmail.com>
2023-10-26 11:06:26 +0300

    reset form handled via state programmatically

--- forms-and-user-input/src/components/StateLogin.jsx
+++ forms-and-user-input/src/components/StateLogin.jsx
@@ -8,7 +8,13 @@ export default function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
     console.log(enteredValues);
+    
+    setEnteredValues({
+      email: '',
+      password: ''
+    });
   }
 
   function handleInputChange(identifier, value) {
