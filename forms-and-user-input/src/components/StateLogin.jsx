09853e95a76c4de856df28b5c33175df46c6bf97
yana.radeva <yana.radeva7@gmail.com>
2023-10-26 11:37:42 +0300

    combine validation on every key stroke with validation upon lost focus

--- forms-and-user-input/src/components/StateLogin.jsx
+++ forms-and-user-input/src/components/StateLogin.jsx
@@ -5,6 +5,13 @@ export default function Login() {
     email: '',
     password: ''
   });
+  
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -22,6 +29,18 @@ export default function Login() {
       ...prevValues,
       [identifier]: value
     }));
+
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
   }
 
   return (
@@ -35,9 +54,13 @@ export default function Login() {
             id="email" 
             type="email" 
             name="email" 
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email} 
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
