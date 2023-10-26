9281aa4c07db2a2db58ffa206a6d29f39e4b2fd5
yana.radeva <yana.radeva7@gmail.com>
2023-10-26 15:58:53 +0300

    add components and formatting util function

---
+++ food-order/src/components/Meals.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+import MealItem from './MealItem';
+
+export default function Meals() {
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() => {
+        async function fetchMeals() {
+            try {
+                const response = await fetch('http://localhost:3000/meals');
+                
+                if (!response.ok) {
+                    throw new Error();
+                }
+
+                const meals = await response.json();
+                setLoadedMeals(meals);             
+            } catch (error) {
+                setLoadedMeals([]);
+            }
+        }
+
+        fetchMeals();
+    }, []);
+
+    return (
+      <ul id="meals">
+        {loadedMeals.map((meal) => (
+          <MealItem key={meal.id} meal={meal} />
+        ))}
+      </ul>
+    );
+}
