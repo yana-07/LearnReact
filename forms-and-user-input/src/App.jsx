af17110ccb5085b536b6ba4021ffe8c6ab91c003
yana.radeva <yana.radeva7@gmail.com>
2023-10-26 11:58:13 +0300

    validate input upon form submission

--- forms-and-user-input/src/App.jsx
+++ forms-and-user-input/src/App.jsx
@@ -1,13 +1,12 @@
 import Header from './components/Header.jsx';
-import Signup from './components/Signup.jsx';
-import StateLogin from './components/StateLogin.jsx'
+import Login from './components/Login.jsx'
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Login />
       </main>
     </>
   );
--- forms-and-user-input/src/components/Login.jsx
+++ forms-and-user-input/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,7 +12,15 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes('@');
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Sending http request...");
   }
 
   return (
@@ -26,6 +36,9 @@ export default function Login() {
             name="email"
             ref={email} 
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
