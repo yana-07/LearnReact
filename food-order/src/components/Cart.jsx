dba1dabd9a669933b107d05c1f995028a98369ec
yana.radeva <yana.radeva7@gmail.com>
2023-10-27 11:02:32 +0300

    create CartItem component; work on adding and removing cart items logic

--- food-order/src/components/Cart.jsx
+++ food-order/src/components/Cart.jsx
@@ -3,8 +3,9 @@ import { useContext } from 'react';
 import Modal from "./UI/Modal.jsx";
 import Button from './UI/Button.jsx';
 import CartContext from "../store/CartContext.jsx";
-import { currencyFormatter } from '../util/formatting.js';
 import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
+import { currencyFormatter } from '../util/formatting.js';
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
@@ -20,7 +21,16 @@ export default function Cart() {
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
-                {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
+                {cartCtx.items.map(item => 
+                    <CartItem 
+                        key={item.id} 
+                        name={item.name} 
+                        quantity={item.quantity} 
+                        price={item.price}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
+                )}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotalPrice)}</p>
             <p className="modal-actions">
