e75f63ca582765f93be9b92e7076086c78b80272
yana.radeva <yana.radeva7@gmail.com>
2023-10-30 15:41:12 +0200

    work with multiple state properties

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -6,6 +6,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   // automatically a subscription is set up to the redux store for the current component
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const dispatch = useDispatch();
 
@@ -21,12 +22,14 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={() => increaseHandler(5)}>Increase by 5</button>
--- redux-app/src/store/index.js
+++ redux-app/src/store/index.js
@@ -1,21 +1,33 @@
 const { createStore } = require('redux');
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
+            ...state,
             counter: state.counter + 1
         };
     }
 
     if (action.type === 'increase') {
         return {
+            ...state,
             counter: state.counter + action.amount
         };
     }
 
     if (action.type === 'decrement') {
         return {
+            ...state,
             counter: state.counter - 1
+        };
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            ...state,
+            showCounter: !state.showCounter
         };
     }
 
