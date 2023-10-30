f9c83db9e9f7a00068cf572dfce22ca678cb8ae1
yana.radeva <yana.radeva7@gmail.com>
2023-10-30 15:20:47 +0200

    attach payload to action

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -1,75 +1,82 @@
-import { Component } from 'react';
+//import { Component } from 'react';
 import { useSelector, useDispatch, connect } from 'react-redux';
 
 import classes from './Counter.module.css';
 
-// const Counter = () => {
-//   // automatically a subscription is set up to the redux store for the current component
-//   const counter = useSelector(state => state.counter);
+const Counter = () => {
+  // automatically a subscription is set up to the redux store for the current component
+  const counter = useSelector(state => state.counter);
 
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-//   const incrementHandler = () => {
-//     dispatch({ type: 'increment' });
-//   };
-
-//   const decrementHandler = () => {
-//     dispatch({ type: 'decrement' });
-//   };
-
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//       <button onClick={incrementHandler}>Increment</button>
-//       <button onClick={decrementHandler}>Decrement</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
-// };
-
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
-
-  decrementHandler() {
-    this.props.decrement();
-  }
-
-  toggleCounterHandler() {}
-
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-        <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-        <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    counter: state.counter
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
   };
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch({ type: 'increment' }),
-    decrement: () => dispatch({ type: 'decrement' })
+  const increaseHandler = (amount) => {
+    dispatch({ type: 'increase', amount })
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
   };
-}
 
-// React-redux will still set up a subscription and manage it
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={() => increaseHandler(5)}>Increase by 5</button>
+      <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//         <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//         <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     decrement: () => dispatch({ type: 'decrement' })
+//   };
+// }
+
+// // React-redux will still set up a subscription and manage it
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
