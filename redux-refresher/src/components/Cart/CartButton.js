e1582e1ed34ae968424774c1b022f298b15456f0
yana.radeva7 <yana.radeva7@gmail.com>
2023-11-19 17:15:57 +0200

    use react-redux and redux toolkit to handle cart and UI state

--- redux-refresher/src/components/Cart/CartButton.js
+++ redux-refresher/src/components/Cart/CartButton.js
@@ -1,10 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle()); // toggle is an action creator method which returns an action object
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
